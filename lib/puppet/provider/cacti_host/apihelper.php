725ef607c445baccf27071f77fe46f97ac39fa30
Stefan Schulte <stefan.schulte@taunusstein.net>
2012-08-11 00:56:34 +0200

    Add the ability to pretty print json output
    The apihelper.php script can also be used as a normal user to query
    hosts without using the web application. Using apihelper.php does also
    make sense to debug problems. In this cases it is a lot easier to
    understand the JSON output if it is spread on multiple lines with proper
    indention.

--- lib/puppet/provider/cacti_host/apihelper.php
+++ lib/puppet/provider/cacti_host/apihelper.php
@@ -3,40 +3,51 @@
 
 # Author: Stefan Schulte
 #
-# The apihelper intends to be a little helper script used by the api cact_host
-# provider to create/destroy/modify cacti hosts.
+# This is a helper script used by the provider "api" of the custom type
+# "cacti_host". It can also be used as a stand-alone program to query cacti
+# hosts.
 #
-# Instead of modifying the cacti database directly by running some fancy SQL
-# statements the api provider intends to use the API offered by cacti itself
-# to be independent from any database structur. The API however is only
-# available for PHP while the provider is written in ruby (like puppet itself)
+# The api provider tries to use the cacti API whenever possible so the
+# provider does not have to care about a specific database backend nor does it
+# have to know the username and password of the database backend as all
+# connections are handled through cacti functions.
 #
-# The apihelper acts as a command line tool, passing commandline arguments to
-# the appropiate function calls to the cacti api. Please note that the
-# apihelper doesn't implement any value validation whatsoever and leaves that
-# for the API.
+# There is one problem: Cacti and its API is written in PHP while the puppet
+# plugin is written in ruby (like the rest of puppet). That's why the
+# apihelper.php script is needed:
 #
-# The script does query the API for current cacti_hosts so the result can be
-# used to implement prefetching in the puppet provider. To pass the results of
-# a query back to the caller (the puppet provider in this case), the script
-# will print the results on stdout in JSON format.
+# 1) When puppet wants to query cacti_hosts it does so by calling the
+#    apihelper.php script and the apihelper.php script will return information
+#    about cacti hosts in json format that can be parsed by the api provider
+#    that is written in ruby
+# 2) When puppet wants to create/update a cacti_host it does also call the
+#    apihelper.php script feeding it with a hostname and a json file with all
+#    the cacti attributes
+#
 #
 # NOTE:
+#
 # You *have* to modify a few paths here depending on how you installed php and
 # cacti. The first thing you should check is the shebang line (the first line)
-# if it matches your php executable. The second one are the include paths you
-# need to adjust depending on where the api resides on the filesystem.
+# if it matches your php executable.
+# The second thing you need to modify are the include paths below that have to
+# match your cacti installation path.
 #
 # The best way to check whether the script can find the cacti api is by
 # running
 #
-#     % ./apihelper.php instances /tmp/apitest.txt
-#     % json_pp < /tmp/apitest
+#     % ./apihelper.php instances
 #
-# json_pp is used to pretty-print the file and is part of perl-core/JSON-PP.
-# You can also use cat. The file /tmp/apitest.txt should show a list of all
-# hosts with the corresponding puppet properties.
-
+# You should see all your cacti hosts and their attributes as JSON output on
+# STDOUT now. If cacti logs a lot of warnings on STDOUT (older cacti versions
+# can cause a lot of deprecations warnings if used together with a recent php
+# version) you can also ask the apihelper script to write the JSON
+# output to a file:
+#
+#     % ./apihelper.php instances /tmp/cacti_test.txt
+#     % json_pp < /tmp/cacti_test.txt (requires perl-core/JSON-PP)
+#
+#
 # You may have to edit these to match your cacti installation
 require('/var/www/localhost/htdocs/cacti/include/global.php');
 require_once($config["base_path"]."/lib/api_automation_tools.php");
@@ -79,6 +90,9 @@ $MAP_API2PUPPET=array(
   'max_oids' => 'snmp_max_oids',
 );
 
+/* instances - query all cacti hosts and return host attributes as a nested
+     hash of the form hash[$hostname][$attributename] = $attributevalue
+   @returns - cacti hosts as a nested hash */
 function instances(){
   global $HOST_FIELDS, $MAP_API2PUPPET;
 
@@ -105,6 +119,72 @@ function instances(){
   return $result;
 }
 
+/* json_pp - Pretty prints json data (useful when writing to stdout)
+  @arg $input - the unformated json string as returned by json_encode
+  @returns - the json string with proper indention */
+function json_pp($input) {
+  $output = "";
+  $inside_string = false;
+  $indent = 0;
+  $next_is_escaped = false;
+
+
+  for($i = 0; $i < strlen($input); $i++) {
+    $char = $input[$i];
+
+    if(!$next_is_escaped and $char == "\\") {
+      $next_is_escaped = true;
+      $output .= $char;
+    }
+    else {
+      if($next_is_escaped) {
+        $output .= $char;
+        $next_is_escaped = false;
+      }
+      elseif($inside_string and $char != '"') {
+        $output .= $char;
+      }
+      else {
+        switch($char) {
+        case '{':
+        case '[':
+          $indent += 4;
+          $output .= $char."\n".(str_repeat(" ",$indent));
+          break;
+        case '}':
+        case ']':
+          $indent -= 4;
+          $output .= "\n".(str_repeat(" ",$indent)).$char;
+          break;
+        case ':':
+          $output .= $char." ";
+          break;
+        case ",":
+          $output .= $char."\n".(str_repeat(" ",$indent));
+          break;
+        case '"':
+          $inside_string = !$inside_string;
+          $output .= $char;
+          break;
+        case " ":
+        case "\t":
+        case "\n":
+          break;
+        default:
+          $output .= $char;
+        }
+      }
+    }
+  }
+  return $output;
+}
+
+/* save - updates or creates a host by using the save method of the cacti API
+   @arg $host - The hostname (know as the description in cacti) you want to
+     create or update
+   @arg $attributes - A hash with the host attributes
+   @returns - the hostid of the newly created or updated host. If there was
+     an error the function will return zero */
 function save($host, $attributes) {
   # We first need the id of our host if the host is already present.
   $id;
@@ -180,6 +260,7 @@ function save($host, $attributes) {
 $parms = $_SERVER["argv"];
 array_shift($parms);
 
+
 if (sizeof($parms)) {
   switch($parms[0]) {
   case 'instances':
@@ -191,9 +272,7 @@ if (sizeof($parms)) {
       echo "Instances written to $filename\n";
     }
     else {
-      echo "You have to pass a filename as a second parameter\n";
-      echo "Usage: ".__FILE__." instances <filename>\n";
-      exit(1);
+      echo json_pp(json_encode(instances()));
     }
     break;
   case 'save':
